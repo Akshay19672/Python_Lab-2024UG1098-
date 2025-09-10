a8b3f2820fc7072f247334bbf22cee808bbfdd77
Akshay19672 <121157314+Akshay19672@users.noreply.github.com>
2025-09-10 12:08:43 +0530

    Update experiment_1_Task_3.py

--- experiment_1_Task_3.py
+++ experiment_1_Task_3.py
@@ -1,3 +1,7 @@
+#Name - Akshay Pratap Singh
+#Reg No. - 2024UG1098
+#Course : CS 2101 - Python Programming Lab
+
 # Write a Program to calculate the area and perimeter of a rectangle 
 
 length=int(input("Enter the length of rectangle"))
@@ -5,3 +9,4 @@ breadth=int(input("Enter the breadth of rectangle"))
 
 print("The area of rectangle is :", length*breadth)
 print("The Perimeter of rectangle is :",2*(length + breadth))
+
